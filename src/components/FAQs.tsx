a072b41a73cd51fe8407b37ac9437f2a3d3c60cc
Muhammad Faisal <faisalgojra339@gmail.com>
2024-11-11 21:52:55 +0500

    modify the footer according to the design

--- src/components/FAQs.tsx
+++ src/components/FAQs.tsx
@@ -1,6 +1,9 @@
+"use client"
 import React from 'react'
 import PlusIcon from "../assets/icons/plus.svg"
+import MinusIcon from "../assets/icons/minus.svg"
 
+import clsx from 'clsx';
 const items = [
   {
     question: "What payment methods do you accept?",
@@ -23,24 +26,41 @@ const items = [
       "Security is our top priority. We use state-of-the-art encryption and comply with the best industry practices to ensure that your data is stored securely and accessed only by authorized users.",
   },
 ];
-function FAQ() {
-  return (
-    <div className='bg-black text-white bg-gradient-to-t'>
-      <div className='container'>
-<h2>Frequently asked questions</h2>
 
 
-      </div>
-     {items.map(({question, answer})=>(
-      <div key={question}>
-        <div>
-          <span>{question} <PlusIcon/></span>
+const AccordingItem = ({question, answer} : {question: string; answer: string;})=>{
+  const [isOpen, setIsOpen] = React.useState(false);
+  return(
+    <div  className='py-7 border-b  border-white/30' onClick={()=> setIsOpen(!isOpen)}>
+
+        <div className='flex items-center '>
+          <span className='flex-1 text-lg font-bold'>{question} </span>
+          {isOpen? <MinusIcon/> : <PlusIcon/>}
+          
         </div>
-        <div className='hidden'>
+
+
+        <div className={clsx("mt-4",{"hidden" : !isOpen, "" : isOpen === true})}>
           {answer}
         </div>
       </div>
+  )
+}
+
+
+function FAQ() {
+  return (
+    <div className='bg-black text-white bg-gradient-to-t from-[#5D2CA8] to-black py-[72px] sm:py-24'>
+      <div className='container'>
+<h2 className='text-center text-5xl sm:text-6xl sm:max-w-[648px] font-bold tracking-tighter mx-auto'>Frequently asked questions</h2>
+<div className='mt-12 max-w-[648px] mx-auto'>
+{items.map(({question, answer})=>(
+    <AccordingItem  key={question} question={question} answer={answer} />
      ))}
+</div>
+
+      </div>
+    
     </div>
   )
 }
